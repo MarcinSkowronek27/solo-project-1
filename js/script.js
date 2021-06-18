2753642bc30c83d3f2f4effd55ac77c62b4f00f7
Marcin Skowronek <marcinfoto35@op.pl>
2021-06-18 21:42:37 +0200

    Solved shifted hamburger button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 function toggleMenu(visible) {
     document.querySelector('.sidebar-wrapper-all').classList.toggle('show', visible)
+    document.querySelector('.hamburger').classList.toggle('shifted', visible)
 }
 
 document.querySelector('.hamburger').addEventListener('click', function (e) {
